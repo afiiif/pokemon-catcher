0b313346b8820a4b18157d1aefa1f17872e0f258
Muhammad Afifudin <afif.x.udin@gmail.com>
2021-08-29 06:41:57 +0700

    Update about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,41 +9,48 @@ export default function AboutPage() {
       </h1>
       <div className="md:text-xl max-w-3xl pt-8 text-gray-700">
         <p className="pb-5">
-          Pokémon Catcher is a try-your-luck game
+          Pokémon Catcher is a simple pokemon catching game
           where you will always have 50% chance to catch any pokémon.
-          You can catch as much as you want.
-          Have fun!
+          You can catch as much as you want. Enjoy!
         </p>
         <p>
-          Your pokémon will be saved in your browser's local storage.
+          Note: Your pokémon will be saved in your browser's local storage.
           If you clear the local storage, you will lose your data.
         </p>
       </div>
 
-      <h2 className="h2 pt-8">
+      <h2 className="h2 pt-8 md:pt-12">
         Tech-Stack
       </h2>
       <div className="md:text-xl max-w-3xl pt-2 md:pt-4 text-gray-700">
         <ul className="list-disc pl-8">
-          <li><a className="link" href="https://nextjs.org/">Next.js</a> <span className="text-gray-400">&nbsp;&mdash; Using ISR strategy</span></li>
+          <li><a className="link" href="https://nextjs.org/">Next.js</a> <span className="text-gray-400">&mdash; Using ISR strategy</span></li>
           <li><a className="link" href="https://react-query.tanstack.com/">React Query</a></li>
           <li><a className="link" href="https://react-hook-form.com/">React Hook Form</a></li>
           <li><a className="link" href="https://tailwindcss.com/">Tailwind CSS</a></li>
         </ul>
       </div>
 
-      <h2 className="h2 pt-8">
+      <h2 className="h2 pt-8 md:pt-12">
         Github
       </h2>
       <div className="md:text-xl max-w-3xl pt-2 md:pt-4 text-gray-700">
         <a className="link" href="https://github.com/afiiif/pokemon-catcher">https://github.com/afiiif/pokemon-catcher</a>
       </div>
 
-      <h2 className="h2 pt-8">
+      <h2 className="h2 pt-8 md:pt-12">
         Author
       </h2>
       <div className="md:text-xl max-w-3xl pt-2 md:pt-4 text-gray-700">
         <a className="link" href="https://www.linkedin.com/in/m-afifudin/">Muhammad Afifudin</a>
+      </div>
+
+      <h2 className="h2 pt-8 md:pt-12">
+        Credit
+      </h2>
+      <div className="md:text-xl max-w-3xl pt-2 md:pt-4 text-gray-700">
+        Some icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a>
+        {' '}from <a className="link" href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
       </div>
 
     </DefaultLayout>
