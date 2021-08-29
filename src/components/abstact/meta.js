5336efffc675deb7099345df0fb2bb1a43e40a4f
Muhammad Afifudin <afif.x.udin@gmail.com>
2021-08-29 08:00:56 +0700

    Add favicon

--- src/components/abstact/meta.js
+++ src/components/abstact/meta.js
@@ -9,6 +9,7 @@ export const THEME_COLOR = '#3b82f6';
 export default function Meta() {
   return (
     <Head>
+
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       <meta name="author" content={AUTHOR} />
@@ -19,6 +20,24 @@ export default function Meta() {
       <meta property="og:description" content={DESCRIPTION} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content="https://pokemoncatcher.vercel.app/images/pokemon-catcher-preview.png" />
+
+      <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content="/favicons/ms-icon-144x144.png" />
+
     </Head>
   );
 }
