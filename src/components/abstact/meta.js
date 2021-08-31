0e57b42aeef82b3dfe834268145d0058ee4d2b9f
Muhammad Afifudin <afif.x.udin@gmail.com>
2021-08-31 12:11:40 +0700

    Remove meta viewport
    Use default meta viewport without maximum-scale=1.0 for accessibility

--- src/components/abstact/meta.js
+++ src/components/abstact/meta.js
@@ -11,7 +11,6 @@ export default function Meta() {
     <Head>
 
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
       <meta name="author" content={AUTHOR} />
       <meta name="keywords" content={KEYWORDS} />
       <meta name="theme-color" content={THEME_COLOR} />
