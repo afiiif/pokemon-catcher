3aacb8924e45fe4529749a8d8564cf96a89218dc
Muhammad Afifudin <afif.x.udin@gmail.com>
2021-09-18 12:33:48 +0700

    Improve capitalizeFirstLetter function

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,3 +1,5 @@
-export const capitalizeFirstLetter = (string = '') => (
-  string.charAt(0).toUpperCase() + string.slice(1)
+export const capitalizeFirstLetter = (string) => (
+  typeof string === 'string'
+    ? string.charAt(0).toUpperCase() + string.slice(1)
+    : ''
 );
